63fa2e2b5ccd3f99e3a626cd0962fef4f4cfe4f9
christianmandia <christianmandia@gmail.com>
2018-10-29 16:43:35 +0100

    Initial commit

---
+++ app/src/main/java/cifprodolfoucha/com/listapp/Activity_ListaDefault.java
@@ -0,0 +1,13 @@
+package cifprodolfoucha.com.listapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity_ListaDefault extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__lista_default);
+    }
+}
