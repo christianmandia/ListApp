6a97375786c704bc5bbe8edb1eba1671c56e8a6f
christianmandia <christianmandia@gmail.com>
2018-10-30 20:51:40 +0100

    +ListaDefault
    Merge branch 'master' of https://github.com/christianmandia/ListApp into Christian
    
    # Conflicts:
    #	app/src/main/res/values-en/strings_registro_controis.xml
    #	app/src/main/res/values-es/strings_registro_controis.xml
    #	app/src/main/res/values-gl/strings_registro_controis.xml

--- app/src/main/java/cifprodolfoucha/com/listapp/Activity_Ajustes.java
+++ app/src/main/java/cifprodolfoucha/com/listapp/Activity_Ajustes.java
@@ -8,6 +8,6 @@ public class Activity_Ajustes extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity__ajustes);
+        setContentView(R.layout.layout__ajustes);
     }
 }
