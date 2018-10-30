f6efc8c90bd6cf23b33f2f1a4257e4932a07be60
christianmandia <christianmandia@gmail.com>
2018-10-30 17:58:34 +0100

    .

--- app/src/main/java/cifprodolfoucha/com/listapp/Login.java
+++ app/src/main/java/cifprodolfoucha/com/listapp/Login.java
@@ -3,11 +3,11 @@ package cifprodolfoucha.com.listapp;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class Login extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
