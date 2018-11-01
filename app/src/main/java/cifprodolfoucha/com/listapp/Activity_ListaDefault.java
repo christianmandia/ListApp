e6797c0b8d5f8dbeb10ea16e2094838216905259
christianmandia <christianmandia@gmail.com>
2018-11-01 18:34:24 +0100

    +AdaptadorPrueba listadefault

--- app/src/main/java/cifprodolfoucha/com/listapp/Activity_ListaDefault.java
+++ app/src/main/java/cifprodolfoucha/com/listapp/Activity_ListaDefault.java
@@ -6,12 +6,13 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Activity_ListaDefault extends Activity {
 
-
+/*
     private void cargarListas(){
         //Fonte de datos
         String[] froitas = new String[] { "Pera", "Mazá", "Plátano" };
@@ -23,9 +24,26 @@ public class Activity_ListaDefault extends Activity {
         //Enlace do adaptador co ListView
         lvFroitas.setAdapter(adaptador);
     }
+
+*/
+    private void cargarLista(){
+        String[] articulos={"pilas AA","articulo2","mazá","articulo4","articulo5","articulo6"};
+        int[] cantidad={1,2,10,0,0,4};
+        double[] precio={0.5,15,30,0,20,0};
+
+
+        ListView lista = findViewById(R.id.lvElementosLista_ListaDefault);
+
+        Adapatador_ListaDefault meuAdaptador = new Adapatador_ListaDefault(this,articulos,cantidad,precio);
+        lista.setAdapter(meuAdaptador);
+
+    }
+
+
+
     private void xestionarEventos(){
 
-        ListView lvFroitas = findViewById(R.id.lvElementosLista_ListaDefault);
+ /*       ListView lvFroitas = findViewById(R.id.lvElementosLista_ListaDefault);
         //Escoitador
         lvFroitas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
@@ -37,6 +55,10 @@ public class Activity_ListaDefault extends Activity {
 
             }
         });
+ */
+
+
+
     }
 
 
@@ -44,7 +66,7 @@ public class Activity_ListaDefault extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout__listadefault);
-        cargarListas();
+        cargarLista();
         xestionarEventos();
     }
 
--- app/src/main/java/cifprodolfoucha/com/listapp/Adapatador_ListaDefault.java
+++ app/src/main/java/cifprodolfoucha/com/listapp/Adapatador_ListaDefault.java
@@ -1,7 +1,13 @@
 package cifprodolfoucha.com.listapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Adapatador_ListaDefault extends ArrayAdapter {
 
@@ -18,5 +24,55 @@ public class Adapatador_ListaDefault extends ArrayAdapter {
         this.cantidad = cantidad;
         this.precio = precio;
         this.mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        //return super.getCount();
+        return articulos.length;       // Poderíamos poñer tamén o número de imaxes.
+    }
+
+    @Override
+    public View getDropDownView(int position,  View convertView,  ViewGroup parent) {
+        return getView(position,convertView,parent);
+    }
+
+    private static class ViewHolder {
+        CheckedTextView mCheck;
+        TextView mCantidad;
+        TextView mPrecio;
+    }
+
+    @Override
+    public View getView(int position, View convertView,  ViewGroup parent) {
+        //return super.getView(position, convertView, parent);
+
+        Adapatador_ListaDefault.ViewHolder viewFila = new Adapatador_ListaDefault.ViewHolder();
+        if (convertView == null) {
+            LayoutInflater mInflater = (LayoutInflater) mContext.
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = mInflater.inflate(R.layout.layout_elemento_listadefault2, parent, false);
+            viewFila.mCheck = (CheckedTextView) convertView.findViewById(R.id.ctvNombreArticulo);
+            viewFila.mCantidad = (TextView) convertView.findViewById(R.id.tvCantidadArticuloLista);
+            viewFila.mPrecio = (TextView) convertView.findViewById(R.id.tvPrecioArticuloLista);
+            convertView.setTag(viewFila);
+        } else {
+            viewFila = (Adapatador_ListaDefault.ViewHolder) convertView.getTag();
+        }
+        viewFila.mCheck.setText(articulos[position]);
+
+        if(cantidad[position]!=0) {
+            viewFila.mCantidad.setText(cantidad[position]+"");
+        }else{
+            viewFila.mCantidad.setText("1");
+        }
+
+        if(precio[position]!=0) {
+            viewFila.mPrecio.setText(precio[position] + "€");
+        }else{
+            viewFila.mPrecio.setText("");
+        }
+
+        return convertView;
     }
 }
